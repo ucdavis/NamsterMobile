620e4df147a3d9a9853e36ebb43fd332d1145044
Scott Kirkland <srkirkland@gmail.com>
2015-12-21 11:53:03 -0800

    add infinite scroll for initial query

--- app/NamList.js
+++ app/NamList.js
@@ -13,6 +13,7 @@ var {
 } = React;
 
 var NamList = React.createClass({
+  fetchingData: false, //static so we don't fetch data multiple times
   getInitialState: function() {
     return {
       dataList: new ListView.DataSource({
@@ -25,7 +26,10 @@ var NamList = React.createClass({
     };
   },
   componentWillMount() {
-    console.log('mounted', this.props.query);
+    this.setState({loading: true});
+    this.loadNams();
+  },
+  loadNams() {
     if (this.props.query){
       this.fetchData();
     } else if (this.props.filter){
@@ -34,28 +38,43 @@ var NamList = React.createClass({
   },
   filterData: function() {
     var self = this;
-    self.setState({nams: [], loading: true});
+    self.fetchingData = true;
 
     SearchFactory.fetchFilteredData(self.props.filter, function(data){
+      self.fetchingData = false;
       self.setState({
-        nams: data.hits.hits,
+        nams: self.state.nams.concat(data.hits.hits),
+        totalHits: data.hits.total,
+        page: self.state.page + 1,
         loading: false
       });
     });
   },
   fetchData: function() {
     var self = this;
-    self.setState({nams: [], loading: true});
+    self.fetchingData = true;
 
-    SearchFactory.fetchQueryData(self.props.query, function(data){
+    SearchFactory.fetchQueryData(self.props.query, self.state.page, self.state.pageSize, function(data){
+      self.fetchingData = false;
       self.setState({
-        nams: data.hits.hits,
+        nams: self.state.nams.concat(data.hits.hits),
+        totalHits: data.hits.total,
+        page: self.state.page + 1,
         loading: false
       });
     });
   },
   onSelected: function(nam){
     this.props.onSelected(nam);
+  },
+  onEndReached: function(){
+    console.log('on end reached called');
+    if (this.state.nams.length < this.state.totalHits && !this.fetchingData){
+      this.fetchingData = true;
+      //if there are still results out there, grab another pageSize worth
+      console.log('going to next page');
+      this.loadNams();
+    }
   },
   renderLoading: function(){
     return (
@@ -80,6 +99,7 @@ var NamList = React.createClass({
       <View style={this.props.query ? styles.searchContainer : styles.navigationContainer}>
         <ListView
           dataSource={dataSource}
+          onEndReached={this.onEndReached}
           renderRow={(nam)=><NamCell nam={nam} onSelected={this.onSelected} />}
           style={styles.listView}
         />
